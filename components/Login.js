256c1f8218344c56d3e26feedcc99e6c4304c068
moad6039 <muaadh.al.ammari@gmail.com>
2026-02-12 17:13:19 +0100

    Login composant structure presque fini avant ajout import signup

--- components/Login.js
+++ components/Login.js
@@ -2,29 +2,36 @@ import styles from "../styles/Login.module.css";
 import Image from "next/image";
 import Link from "next/link";
 
-
 function Login() {
   return (
     <div>
       <main className={styles.main}>
         <div className={styles.logo}>
-           <Image
-              src="/Twitter_logo.png"
-              alt="Logo-twitter"
-              width={100}
-              height={100}
-            />
+          <Image
+            src="/Twitter_logo.png"
+            alt="Logo-twitter"
+            width={100}
+            height={100}
+          />
         </div>
         <div className={styles.title}>
           <h1>See what's happening</h1>
         </div>
         <div className={styles.sentence}>
-          <p>Join Hackatweet today</p>
+          <p>
+            <strong>Join Hackatweet today</strong>
+          </p>
         </div>
         <div className={styles.buttonGroup}>
-          <button href="/" className={styles.signupLink}>Sign up</button>
-          <p>Already have an account ?</p>
-          <button href="/"className={styles.signinLink}>Sign in</button>
+          <button href="/" className={styles.signupButton}>
+            Sign up
+          </button>
+          <p className={styles.sentence2}>
+            <strong>Already have an account ?</strong>
+          </p>
+          <button href="/" className={styles.signinButton}>
+            Sign in
+          </button>
         </div>
       </main>
     </div>
