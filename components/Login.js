57a921a82f9558bc735d8d95e3004c1b15d39c58
moad6039 <muaadh.al.ammari@gmail.com>
2026-02-12 16:51:47 +0100

    composant Login created

--- components/Login.js
+++ components/Login.js
@@ -1,9 +1,34 @@
 import styles from "../styles/Login.module.css";
+import Image from "next/image";
+import Link from "next/link";
+
 
 function Login() {
-  return Login;
-
-  
+  return (
+    <div>
+      <main className={styles.main}>
+        <div className={styles.logo}>LOGO
+           <Image
+              src="/Twitter_logo.png"
+              alt="Planifier"
+              width={100}
+              height={100}
+            />
+        </div>
+        <div className={styles.title}>
+          <h1>See what's happening</h1>
+        </div>
+        <div className={styles.sentence}>
+          <p>Join Hackatweet today</p>
+        </div>
+        <div className={styles.buttonGroup}>
+          <Link href="/" className={styles.signupLink}>Sign up</Link>
+          <p>Already have an account ?</p>
+          <Link href="/"className={styles.signinLink}>Sign in</Link>
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default Login;
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,12 @@
-import '../styles/globals.css';
-import Head from 'next/head';
+import "../styles/globals.css";
+import Head from "next/head";
+import Login from "../components/Login";
 
 function App({ Component, pageProps }) {
   return (
     <>
+     <Login />
       <Head>
-        <title>Next.js App</title>
       </Head>
       <Component {...pageProps} />
     </>
