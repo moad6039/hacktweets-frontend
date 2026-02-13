070d39fdab22f040dd44a7c813956772534757f6
loicvoireau <loicvoireau3@gmail.com>
2026-02-13 14:13:33 +0100

    modification page accueil

--- components/Home.js
+++ components/Home.js
@@ -1,6 +1,5 @@
 import Tweet from "./Tweet";
 import styles from "../styles/Home.module.css";
-import { useEffect, useState } from "react";
 import Left from "./left";
 import Right from "./Right";
 import Middle from "./Middle";
@@ -9,18 +8,8 @@ function Home() {
   return (
     <div className={styles.container}>
       <Left />
-      <div className={styles.partTwo}>
-          <h1 className={styles.header}>Home</h1>
-          <div className={styles.blockInputTweet}>
-            <input type='text' placeholder="What's up" className={styles.inputTweet} onChange={(e) => setinputTweet(e.target.value)} value={inputTweet} />
-          </div>
-          <div className={styles.blockButtonCharacters}>
-            <span className={styles.numberCharacter}>14/280</span>
-            <button className={styles.tweetButton} onClick={() => handleClickCreateTweet()}>Tweet</button>
-          </div>
-          <div className={styles.tweetBlock}>
-            {tweets}
-          </div>
+      <div className={styles.Middle}>
+        <Middle />
       </div>
       <div className={styles.right}>
         <Right />
