767268335ab74c47ab79eb9dc98f1bb6144b34b4
moad6039 <muaadh.al.ammari@gmail.com>
2026-02-13 14:13:50 +0100

    connexion page

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,7 @@ import Home from "../components/Home";
 import Login from "../components/Login";
 
 function Index() {
-<<<<<<< HEAD
   return <Login />;
-=======
-  return <Home />;
->>>>>>> d234c031595f865d8cfb2dcd86b5f50bcd7ad359
 }
 
 export default Index;
